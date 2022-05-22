cdf1a7de9036f47c45f959861c9d05fa62b9593a
Oladele Oriyomi <marshall_400@yahoo.com>
2022-05-22 21:52:39 +0300

    Exercise 3.9
    Make the backend work with the phonebook frontend from the exercises of the previous part. Do not implement the functionality for making changes to the phone numbers yet, that will be implemented in exercise 3.17.

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const morgan = require ('morgan')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
 
 morgan.token('type', function (req, res) { return JSON.stringify(req.body)})
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :type'))
+app.use(cors())
 
 let persons = [
     { 

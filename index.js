0b335539f9436623496b14b612fab0ea171522f3
Oladele Oriyomi <marshall_400@yahoo.com>
2022-05-22 22:03:02 +0300

    Heroku
    Heroku initial commit

--- index.js
+++ index.js
@@ -108,7 +108,7 @@ const idGenerator = () => {
     return idFinder()
 }
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })

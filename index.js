4263ad471bd17b6c007180e7c0aeb1752bf7626c
Oladele Oriyomi <marshall_400@yahoo.com>
2022-05-22 23:20:17 +0300

    uibuild

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 const app = express()
 
 app.use(express.json())
-
+app.use(express.static('build'))
 morgan.token('type', function (req, res) { return JSON.stringify(req.body)})
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :type'))
 app.use(cors())
